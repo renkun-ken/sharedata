898e247c094c8673727ba5ca9923f8fc1b7bac8f
Kun Ren <renkun@outlook.com>
2015-05-13 15:04:18 +0800

    remove include dynamic_bitset

--- src/types.h
+++ src/types.h
@@ -1,7 +1,6 @@
 #ifndef SHAREDATA_TYPES_H_
 #define SHAREDATA_TYPES_H_
 
-#include <boost/dynamic_bitset.hpp>
 #include <boost/interprocess/managed_shared_memory.hpp>
 #include <boost/interprocess/containers/vector.hpp>
 #include <boost/interprocess/allocators/allocator.hpp>
