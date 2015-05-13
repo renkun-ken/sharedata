85568fbc9618a443b4ed8ff566a9ad3e98e40d5d
Kun Ren <renkun@outlook.com>
2015-05-13 21:30:25 +0800

    add tests

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(sharedata)
+
+test_check("sharedata")
---
+++ tests/testthat/test-package.R
@@ -0,0 +1,19 @@
+context("sharedata")
+
+test_that("share_object", {
+  expect_equal(share_object(1:10, "data1"), TRUE)
+  expect_equal(share_object(letters, "data2"), TRUE)
+  expect_equal(share_object(mtcars, "data3"), TRUE)
+})
+
+test_that("get_object", {
+  expect_identical(get_object("data1"), 1:10)
+  expect_identical(get_object("data2"), letters)
+  expect_identical(get_object("data3"), mtcars)
+})
+
+test_that("remove_object", {
+  expect_equal(remove_object("data1"), TRUE)
+  expect_equal(remove_object("data2"), TRUE)
+  expect_equal(remove_object("data3"), TRUE)
+})
