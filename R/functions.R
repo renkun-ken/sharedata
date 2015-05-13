fddba48530ab0c56bb56cd2d113a51e6b80272b2
Kun Ren <renkun@outlook.com>
2015-05-13 21:30:04 +0800

    add documentation

--- R/functions.R
+++ R/functions.R
@@ -2,7 +2,13 @@
 #' @importFrom Rcpp sourceCpp
 NULL
 
+#' Copy object from shared memory
+#' @param name character. The name of the object.
 #' @export
+#' @examples
+#' \dontrun{
+#' get_object("shared_numbers")
+#' }
 get_object <- function(name) {
   stopifnot(is.character(name))
   raw_data <- get_shared_raw(name, "raw")
@@ -12,7 +18,17 @@ get_object <- function(name) {
   obj
 }
 
+#' Copy object to shared memory
+#' @param x an object.
+#' @param name character. The name of the object.
+#' @param overwrite logical to indicate whether to overwrite
+#' existing memory with the same name.
 #' @export
+#' @examples
+#' \dontrun{
+#' share_object(mtcars, "shared_mtcars")
+#' share_object(rnorm(1000), "shared_rnd_numbers")
+#' }
 share_object <- function(x, name, overwrite = TRUE) {
   stopifnot(is.character(name), is.logical(overwrite))
   conn <- rawConnection(raw(0L), "w")
@@ -23,7 +39,13 @@ share_object <- function(x, name, overwrite = TRUE) {
   res == 0L
 }
 
+#' Remove object from shared memory
+#' @param name character. The name of the object.
 #' @export
+#' @examples
+#' \dontrun{
+#' remove_object("shared_data")
+#' }
 remove_object <- function(name) {
   stopifnot(is.character(name))
   remove_raw(name) == 0L
